d7155052b66ef45a89179b133fb61ee1bbf88719
Alex Cahn <alexcahn@gmail.com>
2017-04-19 14:37:04 -0700

    added all buttons and text fields, just need to make them do stuff

---
+++ Week-6-Lab/Week-6-Lab/AddEmployeeViewController.m
@@ -0,0 +1,44 @@
+//
+//  AddEmployeeViewController.m
+//  Week-6-Lab
+//
+//  Created by A Cahn on 4/19/17.
+//  Copyright © 2017 A Cahn. All rights reserved.
+//
+
+#import "AddEmployeeViewController.h"
+#import "EmployeeDatabase.h"
+
+@interface AddEmployeeViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *firstName;
+@property (weak, nonatomic) IBOutlet UITextField *lastName;
+@property (weak, nonatomic) IBOutlet UITextField *age;
+@property (weak, nonatomic) IBOutlet UITextField *yearsEmployed;
+@property (weak, nonatomic) IBOutlet UITextField *managerName;
+@property (weak, nonatomic) IBOutlet UITextField *email;
+
+- (IBAction)saveButtonPressed:(UIButton *)sender;
+
+- (IBAction)cancelButtonPressed:(UIButton *)sender;
+
+@end
+
+@implementation AddEmployeeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+}
+
+
+
+- (IBAction)saveButtonPressed:(UIButton *)sender {
+}
+
+- (IBAction)cancelButtonPressed:(UIButton *)sender {
+    
+}
+@end
